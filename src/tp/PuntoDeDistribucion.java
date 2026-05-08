14cf9b60cc3c067473cd1a73bbf16c8c48b66939
GO <GP.Professional@proton.me>
2026-05-08 12:53:38 -0300

    Se implementa logica del método distanciaA de la clase PuntoDeDistrubicion

--- src/tp/PuntoDeDistribucion.java
+++ src/tp/PuntoDeDistribucion.java
@@ -19,14 +19,23 @@ public class PuntoDeDistribucion implements Conectable {
     }
     
     protected double distanciaA(PuntoDeDistribucion otro){
-        double distancia=0.0;
+        
+        //obtenemos el valor actual de las coordenadas
+        //y medimos la distancia entre ambas (restamos)
+        double difX = otro.coordenadaX - this.coordenadaX;
+        double difY = otro.coordenadaY - this.coordenadaY;
+        
+        //elevamos al cuadrado cada una
+        //sumamos
+        //y sacamos la raíz
+        double distancia = Math.sqrt(Math.pow(difX, 2) +
+                           Math.pow(difY, 2));
+        
+        //devolvemos la distancia euclidiana
         return distancia;
     }
 
-    @Override
-    public String toString() {
-        return "PuntoDeDistribucion{" + "nombre=" + nombre + ", coordenadaX=" + coordenadaX + ", coordenadaY=" + coordenadaY + ", conectado=" + conectado + ", visitado=" + visitado + '}';
-    }
+ 
 
     @Override
     public boolean conectarALaRed() {
@@ -41,6 +50,11 @@ public class PuntoDeDistribucion implements Conectable {
     @Override
     public String getNombreNodo() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String toString() {
+        return "PuntoDeDistribucion{" + "nombre=" + nombre + ", coordenadaX=" + coordenadaX + ", coordenadaY=" + coordenadaY + ", conectado=" + conectado + ", visitado=" + visitado + '}';
     }
     
     
